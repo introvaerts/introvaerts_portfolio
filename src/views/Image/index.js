2ae619075cf9bad652ca3084a1ba392a41bef4d6
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-11 17:37:55 +0100

    Add image view

--- src/views/Image/index.js
+++ src/views/Image/index.js
@@ -1,5 +1,28 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import SingleImage from '../../shared/components/SingleImage';
+import Api from '../../utils/Api';
+
 const Image = () => {
-  return <div>Image</div>;
+  const imageParam = useParams();
+  let [image, setImage] = useState();
+
+  useEffect(() => {
+    Api.getImage(imageParam.id)
+      .then(response => setImage(response.data.data))
+      .catch(err => console.error(err));
+  }, [imageParam]);
+
+  return (
+    <div>
+      <SingleImage src={image?.image_url} />
+      <h3>
+        {image?.caption.title} {image?.caption.year} {image?.caption.media}{' '}
+        {image?.caption.dimensions}
+      </h3>
+      <p>{image?.description}</p>
+    </div>
+  );
 };
 
 export default Image;
