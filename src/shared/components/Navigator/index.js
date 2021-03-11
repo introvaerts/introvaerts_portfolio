cb5e6b5f19aeb768450c6cffd7188362d5e2a7ba
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-11 17:39:53 +0100

    Remove not needed browser router

--- src/shared/components/Navigator/index.js
+++ src/shared/components/Navigator/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { StyledLink, Menu } from './Styles';
 
 const Navigator = ({ userDetails, translate, clickHandler }) => {
