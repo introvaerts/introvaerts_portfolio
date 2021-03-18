c861bc1730366ad9e5663230209226d5221ba1da
Montaser Amro <mono.amro@gmail.com>
2021-03-18 12:37:51 +0100

    restyles the about_image

--- src/shared/components/SingleImage/Styles.js
+++ src/shared/components/SingleImage/Styles.js
@@ -1,22 +1,13 @@
 import styled from 'styled-components';
 
 export const ImageContainer = styled.div`
-  ${
-    '' /* width: calc( 98vw - 1rem);
-  height: 45vh;
-  display: flex;
-  justify-content: center;
-  margin-bottom: 1rem; */
-  }
-  ${'' /* max-width: 30vw;
-  margin: 0 auto; */}
   width: 100%;
   margin: 2rem auto;
-  height: 45vh;
   text-align: center;
 `;
 
 export const Image = styled.img`
   object-fit: cover;
-  height: 100%;
+  max-height: 45vh;
+  max-width: 100%;
 `;
